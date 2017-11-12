f99c566beaaee954db60f9aa9302a7c7cab6fb4a
Liam Hartigan <liamhartigan@hotmail.com>
2017-11-11 22:15:35 -0500

    changed static settings and wsgi

--- sportacle/settings.py
+++ sportacle/settings.py
@@ -150,9 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-STATIC_DIR,
-]
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
